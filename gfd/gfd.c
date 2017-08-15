0c8a1b6104c6fe5838566f55709cab65a0528735
icchy <icchyr@gmail.com>
2017-08-15 14:39:29 +0900

    fix hook point

--- gfd/gfd.c
+++ gfd/gfd.c
@@ -193,6 +193,7 @@ static int calc_csum(struct sk_buff *skb) {
 
 
 // DNS hook module
+
 static unsigned handle_hook_dns_in(const struct nf_hook_ops *ops,
     struct sk_buff *skb,
     const struct net_device *in,
@@ -282,6 +283,8 @@ static unsigned handle_hook_dns_out(const struct nf_hook_ops *ops,
   return NF_ACCEPT;
 }
 
+
+// TCP hook module
 
 static unsigned handle_hook_tcp_in(const struct nf_hook_ops *ops,
     struct sk_buff *skb,
@@ -386,157 +389,31 @@ static unsigned handle_hook_tcp_out(const struct nf_hook_ops *ops,
 }
 
 
-
-
-static unsigned handle_hook_src(const struct nf_hook_ops *ops,
-    struct sk_buff *skb,
-    const struct net_device *in,
-    const struct net_device *out,
-    int (*okfn)(struct sk_buff*))
-{
-  struct iphdr *iph;
-  struct tcphdr *tcph;
-  struct udphdr *udph;
-
-  uint8_t *tcpdata;
-  uint32_t tcpdatalen;
-  uint32_t saddr;
-
-  // load ip header
-  if((iph = ip_hdr(skb)) == NULL)
-    return NF_ACCEPT;
-
-  // allow except IPv4
-  if(iph->version != 4)
-    return NF_ACCEPT;
-
-  // allow to localhost
-  if(iph->daddr == localhost_eth || iph->daddr == localhost) {
-    return NF_ACCEPT;
-  }
-
-  if(iph->protocol == IPPROTO_TCP && (tcph = tcp_hdr(skb))) {
-    // check if packet includes "ictsc"
-
-    tcpdata = (uint8_t *)tcph + tcph->doff*4;
-    tcpdatalen = ntohs(iph->tot_len) - (iph->ihl*4 + tcph->doff*4);
-
-    if(table_pop(skb)) {
-      // rewrite RST flag
-      tcph->rst = 1;
-      calc_csum(skb);
-      return NF_ACCEPT;
-    }
-
-    if(memmem(tcpdata, tcpdatalen, "ictsc", 5) != NULL) {
-      if(table_push(skb)) {
-        printk(KERN_INFO "GFD: failed to push tcp entry\n");
-      }
-      // rewrite RST flag
-      tcph->rst = 1;
-      calc_csum(skb);
-    }
-  }
-
-  if(iph->protocol == IPPROTO_UDP && (udph = udp_hdr(skb))) {
-    // rewrite dest if port 53
-
-    // allow except udp port 53 (DNS)
-    if(be16_to_cpu(udph->source) != 53)
-      return NF_ACCEPT;
-
-    // print_ipaddr("src_hook src", iph->saddr);
-    // print_ipaddr("src_hook dst", iph->daddr);
-
-    // overwrite src
-    if((saddr = table_pop(skb)) == 0x0) {
-      printk(KERN_INFO "GFD: failed to pop udp entry\n");
-      return NF_ACCEPT;
-    }
-    iph->saddr = saddr;
-    calc_csum(skb);
-  }
-
-  return NF_ACCEPT;
-}
-
-static unsigned handle_hook_dst(const struct nf_hook_ops *ops,
-    struct sk_buff *skb,
-    const struct net_device *in,
-    const struct net_device *out,
-    int (*okfn)(struct sk_buff*))
-{
-  struct iphdr *iph;
-  struct tcphdr *tcph;
-  struct udphdr *udph;
-
-  uint8_t *tcpdata;
-  uint32_t tcpdatalen;
-
-  // load ip header
-  if((iph = ip_hdr(skb)) == NULL)
-    return NF_ACCEPT;
-
-  // allow except IPv4
-  if(iph->version != 4)
-    return NF_ACCEPT;
-
-  // allow from localhost
-  if(iph->saddr == localhost_eth || iph->saddr == localhost) {
-    return NF_ACCEPT;
-  }
-
-  if(iph->protocol == IPPROTO_TCP && (tcph = tcp_hdr(skb))) {
-    // check if packet includes "ictsc"
-
-    tcpdata = (uint8_t *)tcph + tcph->doff*4;
-    tcpdatalen = ntohs(iph->tot_len) - (iph->ihl*4 + tcph->doff*4);
-
-    if(table_pop(skb)) {
-      // rewrite RST flag
-      tcph->rst = 1;
-      return NF_ACCEPT;
-    }
-
-    if(memmem(tcpdata, tcpdatalen, "ictsc", 5) != NULL) {
-      if(table_push(skb)) {
-        printk(KERN_INFO "GFD: failed to push tcp entry\n");
-      }
-      // rewrite RST flag
-      tcph->rst = 1;
-    }
-  }
-
-  if(iph->protocol == IPPROTO_UDP && (udph = udp_hdr(skb))) {
-    // allow except udp port 53 (DNS)
-    if(be16_to_cpu(udph->dest) != 53)
-      return NF_ACCEPT;
-
-    // print_ipaddr("dst_hook src", iph->saddr);
-    // print_ipaddr("dst_hook dst", iph->daddr);
-
-    // overwrite dst
-    if(table_push(skb)) {
-      printk(KERN_INFO "GFD: failed to push udp entry\n");
-    }
-    csum_replace4(&iph->check, iph->daddr, localhost_eth);
-    csum_replace4(&udph->check, iph->daddr, localhost_eth);
-    iph->daddr = localhost_eth;
-  }
-
-  return NF_ACCEPT;
-}
-
+// netfilter hooks
 
 static struct nf_hook_ops hook_dns_in = {
   .hook = handle_hook_dns_in,
   .pf = PF_INET,
-  .hooknum = NF_INET_LOCAL_IN,
+  .hooknum = NF_INET_LOCAL_OUT,
   .priority = NF_IP_PRI_FILTER,
 };
 
 static struct nf_hook_ops hook_dns_out = {
   .hook = handle_hook_dns_out,
+  .pf = PF_INET,
+  .hooknum = NF_INET_PRE_ROUTING,
+  .priority = NF_IP_PRI_FILTER,
+};
+
+static struct nf_hook_ops hook_tcp_in = {
+  .hook = handle_hook_tcp_in,
+  .pf = PF_INET,
+  .hooknum = NF_INET_LOCAL_IN,
+  .priority = NF_IP_PRI_FILTER,
+};
+
+static struct nf_hook_ops hook_tcp_out = {
+  .hook = handle_hook_tcp_out,
   .pf = PF_INET,
   .hooknum = NF_INET_FORWARD,
   .priority = NF_IP_PRI_FILTER,
@@ -562,6 +439,14 @@ int init_module()
     return err;
   }
 
+  if((err = nf_register_hook(&hook_tcp_in)) < 0) {
+    return err;
+  }
+
+  if((err = nf_register_hook(&hook_tcp_out)) < 0) {
+    return err;
+  }
+
   printk(KERN_INFO "GFD: loaded\n");
 
   return 0;
@@ -572,5 +457,7 @@ void cleanup_module()
 {
   nf_unregister_hook(&hook_dns_in);
   nf_unregister_hook(&hook_dns_out);
+  nf_unregister_hook(&hook_tcp_in);
+  nf_unregister_hook(&hook_tcp_out);
   printk(KERN_INFO "GFD: unloaded\n");
 }
